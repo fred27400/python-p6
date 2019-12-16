e7008ad3a5aa70b003a05108f086709f8ebc0c5f
Frédéric <49437574+fred27400@users.noreply.github.com>
2019-12-16 08:14:53 +0100

    Update sauvegardewin.py

--- sauvegardewin.py
+++ sauvegardewin.py
@@ -1,6 +1,12 @@
 #! /usr/bin/python2.7
 # -*- coding:utf-8 -*-
+# Script permettant le lancement du script sauvegarde Windows
+# Auteur : Frederic Seguin
+# Date : 10 Decembre 2019
+# Version : 1.0
 
+
+# import du module
 import os
 
 os.system("ssh Administrateur@192.168.1.2 'py -s' <./sauvegardewin1.py")   # lancez une commande
