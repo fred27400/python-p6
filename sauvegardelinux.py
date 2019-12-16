b81b37f2c86d6077300c803a45391edfe75d4eae
Frédéric <49437574+fred27400@users.noreply.github.com>
2019-12-16 08:12:16 +0100

    Update sauvegardelinux.py

--- sauvegardelinux.py
+++ sauvegardelinux.py
@@ -1,6 +1,12 @@
 #! /usr/bin/python2.7
 # -*- coding:utf-8 -*-
+# Script permettant le lancement du script de sauvegarde Linux
+# Auteur : Frederic Seguin
+# Date : 10 Decembre 2019
+# Version : 1.0
 
+
+# import du module
 import os
 
 os.system("ssh fred@192.168.0.2 'python2.7 -s' <./sauvegardelinux1.py")   # lancez une commande
