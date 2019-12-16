b68f7ec3730735dd0f02b81d0c659b9f03a25703
Frédéric <49437574+fred27400@users.noreply.github.com>
2019-12-16 08:06:21 +0100

    Update listutilsad1.py

--- listutilsad1.py
+++ listutilsad1.py
@@ -1,11 +1,16 @@
 #! /usr/bin/python2.7
 # -*- coding:utf-8 -*-
+# Script permettant la récupération de la liste utilisateurs Windows
+# Auteur : Frederic Seguin
+# Date : 10 Decembre 2019
+# Version : 1.0
+
 
 # import des modules
 import os
 
 # envoi de la commande pour sortir la liste des utilisateurs
 os.system('net user')
-print('list des utilisateurs')
+print('liste des utilisateurs')
 
 
