760b31e326304fc4968fc67c2e55ff1bf294a994
Frédéric <49437574+fred27400@users.noreply.github.com>
2019-12-16 08:03:25 +0100

    Update listutilsad.py

--- listutilsad.py
+++ listutilsad.py
@@ -1,5 +1,10 @@
 #! /usr/bin/python2.7
 # -*- coding:utf-8 -*-
+# Script permettant le lancement du script liste utilisateurs windows
+# Auteur : Frederic Seguin
+# Date : 10 Decembre 2019
+# Version : 1.0
+
 
 import os
 
